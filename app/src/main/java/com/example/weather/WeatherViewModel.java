0d89274c04fdaad4e41b6287414d710362b1233c
m.dorokhin <mdoroxin@yandex.ru>
2019-10-28 10:27:29 +0300

    Правильная асинхронная работа с Retrofit2. Убраны лишние классы. Подчищен импорт.

--- app/src/main/java/com/example/weather/WeatherViewModel.java
+++ app/src/main/java/com/example/weather/WeatherViewModel.java
@@ -1,33 +1,25 @@
 package com.example.weather;
 
-import android.os.AsyncTask;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.databinding.ObservableDouble;
 import androidx.databinding.ObservableField;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.weather.apis.OpenweathermapApi;
 import com.example.weather.apis.models.SixteenDaysWeather;
 import com.example.weather.apis.models.TodayWeather;
-import com.example.weather.configurations.App;
 import com.example.weather.models.DayWeather;
 
-import java.io.IOException;
-import java.security.Timestamp;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.inject.Inject;
-
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WeatherViewModel extends ViewModel {
 
@@ -45,43 +37,27 @@ public class WeatherViewModel extends ViewModel {
 
     private final MutableLiveData<List<DayWeather>> sixteenDayWeathers = new MutableLiveData<>();
 
-    public interface ResultDelegate<T> {
-        public void execute(T value);
-    }
-    public class SendWebRequest<T> extends AsyncTask<Call<T>, Void, T> {
-
-        private ResultDelegate<T> resultDelegate;
-
-        public SendWebRequest(ResultDelegate<T> resultDelegate){
-            this.resultDelegate = resultDelegate;
-        }
-
-        @Override
-        public T doInBackground(Call<T>... request) {
-            T todayWeather = null;
-            try {
-                todayWeather = request[0].execute().body();
-            } catch (IOException e) {
-                Log.e("WeatherViewModel", "Call api trouble", e);
-            }
-            return todayWeather;
-        }
-
-        @Override
-        public void onPostExecute(T todayWeather){
-            this.resultDelegate.execute(todayWeather);
-
-        }
-    }
-
     public WeatherViewModel(OpenweathermapApi api)
     {
         this.api = api;
 
-        Call<TodayWeather> todayWeatherRequest = api.getTodayWeather(cityId, apiKey);
-        new SendWebRequest<TodayWeather>(new ResultDelegate<TodayWeather>() {
+        GetWeather(this.cityId);
+    }
+
+    public LiveData<List<DayWeather>> getSixteenDayWeathers() {
+        return sixteenDayWeathers;
+    }
+
+    /**
+     * Получить погоду для указанного города
+     * @param cityId Идентификатор города
+     */
+    private void GetWeather(int cityId) {
+        api.getTodayWeather(cityId, this.apiKey).enqueue(new Callback<TodayWeather>() {
             @Override
-            public void execute(TodayWeather todayWeather) {
+            public void onResponse(Call<TodayWeather> call, Response<TodayWeather> response) {
+                TodayWeather todayWeather = response.body();
+
                 if (todayWeather != null) {
                     city.set(todayWeather.name);
 
@@ -100,12 +76,18 @@ public class WeatherViewModel extends ViewModel {
                     }
                 }
             }
-        }).execute(todayWeatherRequest);
 
-        Call<SixteenDaysWeather> sixteenDaysRequest = api.getSixteenDayWeather(cityId, apiKey);
-        new SendWebRequest<SixteenDaysWeather>(new ResultDelegate<SixteenDaysWeather>() {
             @Override
-            public void execute(SixteenDaysWeather daysWeathers) {
+            public void onFailure(Call<TodayWeather> call, Throwable t) {
+                Log.w("WeatherViewModel", "Call api trouble", t);
+            }
+        });
+
+        api.getSixteenDayWeather(cityId, this.apiKey).enqueue(new Callback<SixteenDaysWeather>() {
+            @Override
+            public void onResponse(Call<SixteenDaysWeather> call, Response<SixteenDaysWeather> response) {
+                SixteenDaysWeather daysWeathers = response.body();
+
                 List<DayWeather> result = new LinkedList<>();
                 if (daysWeathers != null && daysWeathers.list != null && !daysWeathers.list.isEmpty()) {
                     for (SixteenDaysWeather.ListItem item : daysWeathers.list) {
@@ -130,10 +112,11 @@ public class WeatherViewModel extends ViewModel {
                 }
                 sixteenDayWeathers.postValue(result);
             }
-        }).execute(sixteenDaysRequest);
-    }
 
-    public LiveData<List<DayWeather>> getSixteenDayWeathers() {
-        return sixteenDayWeathers;
+            @Override
+            public void onFailure(Call<SixteenDaysWeather> call, Throwable t) {
+                Log.w("WeatherViewModel", "Call api trouble", t);
+            }
+        });
     }
 }
